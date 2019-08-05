df669406742754a509bcadf202614cd83ba18f3c
971389117 <971389117@qq.com>
2019-08-05 21:07:44 +0800

    更新 readme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'Hello VuePress',
     description: 'Just playing around',
+    base:'/vuepress-demo/',
     themeConfig: {
         lastUpdated: 'Last Updated',
         nav: [{
